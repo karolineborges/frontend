63dddf9ce15b587ef76cb73f9de9dd1a41762fa9
Karoline <tecnologia.karol@gmail.com>
2024-04-22 23:37:07 -0300

    inclusion of home cards

--- frontend/src/components/Container.js
+++ frontend/src/components/Container.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Container = ({ children }) => {
     return (
-        <div style={{ margin: '0 auto', maxWidth: '800px' }}>
+        <div style={{ margin: '0 auto', maxWidth: '100vw' }}>
             {children}
         </div>
     );
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,29 @@
-function Home(){
+import { Link } from "react-router-dom";
+import '../styles/Home.css';
+import  image from "../images/fundo.jpg" ;
 
-    return(
-        <>home page</>
-    )
+function Home(){
+    return (
+        <>
+            <div className="pets-home">
+            <h5>Adote um bichinho</h5>
+            <span>Nos ajude a encontrar um novo lar para eles, adote!</span>
+            <button>
+                <Link to="/adoption" >Pets disponíveis</Link>
+            </button>
+        </div>
+        <div className="cards-home">
+            <div className="card-home">
+
+                <img src={image} />
+            </div>
+                <div className="card-home">
+                <img src={image}/>
+            </div>
+        </div>
+        </>
+
+    );
 }
 
 export default Home;
