98fd8e6387b879ca5f5d8ac3b7ad64f5b97fdf40
Karoline <tecnologia.karol@gmail.com>
2024-05-08 20:04:24 -0300

    change how images are shown

--- src/pages/Adoption.js
+++ src/pages/Adoption.js
@@ -1,16 +1,24 @@
 import "../styles/Adoption.css"
 import React, {useState} from 'react';
 import '../App.css';
-import Images from '../components/Images.js';
-import Controls from '../components/Controls.js';
 import Modal from '../components/Modal.js';
 import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material';
 import AddPet from "../components/AddPet.js";
+import Carousel from "../components/Carousel.js";
+import Filter from "../components/Filter.js";
+import { searchPets } from "../db.js";
 
 
 function Adoption() {
     const [,setModalOpen] = useState(false);
     const [open, setOpen] = useState(false);
+    const [pets, setPets] = useState(searchPets({
+        name: '',
+        vaccinated: '',
+        castrated: '',
+        size: '',
+        type: ''
+    }));
 
     const closeModal = () => {
         setModalOpen(false);
@@ -29,39 +37,51 @@ function Adoption() {
         setOpen(false);
     };
 
+    const handleReturnedPets = (pets) => {
+        setPets(pets);
+    };
     return (
         <div className="adoption">
-            <p>Conheça os pets que estão em busca de um novo lar!</p>
-            <div className="cards">
-                <div className="cards-container">
-                    <Images/>
-               </div>
-                <div className="cards-controls">
-                    <Controls/>
-                </div>
+            <div className="pets-home">
+                <h2>Conheça os pets que estão em busca de um novo lar!</h2>
             </div>
-            <div className="pets-actions">
-                <div className="pets-info">
-                    <div className="pets-description">
-                        <div>Ola, Visitante! Meu nome é Paçoca. Idade: 3 anos. Vacinado: SIM. OBS: Paçoca possui rinite.</div>
+            <Filter onFilter={handleReturnedPets} />
+            <div className="carousel">
+            {
+                pets.length === 0 ? <p style={{margin: "11vh"}} key="" >Não foram encontrados pets para os filtros passados!</p> :
+                <Carousel pets={pets}>
+                    {
+                        pets.map((pet) =>
+                            <img src={pet.image} value={pet.id} key={pet.id} alt={pet.name} />
+                        )
+                    }
+                </Carousel>
+            }
+            </div>
+            {
+                pets.length === 0 ? 
+                    <div className="mini-cards"> <p>Conhece algum animalzinho que esteja aprecisando de abrigo temporário? Faça o cadastro dele <span onClick={handleOpenModal}>AQUI!</span></p></div> :                <div className="pets-actions">
+                    <div className="pets-info">                        
+                        <div className="pets-description">
+                        </div>                        
+                        <Modal closeModal={closeModal} />
                     </div>
-                    <Modal closeModal={closeModal} />
+                    <div className="mini-cards">
+                        <Dialog open={open} onClose={handleCloseModal}>
+                            <DialogTitle style={{ backgroundColor: "#adf0b6", color: "#7B3F00", fontWeight: "bold" }}> Sugestão de Pet </DialogTitle >
+                            <DialogContent style={{ marginTop: "1vw" }}>
+                                <AddPet IsSugestion={true} />
+                            </DialogContent>
+                            <DialogActions>
+                                <Button onClick={handleCloseModal} className='buttonClose'>
+                                    Fechar
+                                </Button>
+                            </DialogActions>
+                        </Dialog>
+                        <p>Conhece algum animalzinho que esteja aprecisando de abrigo temporário? Faça o cadastro dele <span onClick={handleOpenModal}>AQUI!</span></p>
+                    </div>
                 </div>
-                <div className="mini-cards">
-                    <Dialog open={open} onClose={handleCloseModal}>
-                        <DialogTitle style={{ backgroundColor: "#adf0b6", color: "#7B3F00", fontWeight: "bold" }}> Sugestão de Pet </DialogTitle >
-                        <DialogContent style={{ marginTop: "1vw" }}>
-                            <AddPet IsSugestion={true} />
-                        </DialogContent>
-                        <DialogActions>
-                            <Button onClick={handleCloseModal} className='buttonClose'>
-                                Fechar
-                            </Button>
-                        </DialogActions>
-                    </Dialog>
-                    <p>Conhece algum animalzinho que esteja aprecisando de abrigo temporário? Faça o cadastro dele <span onClick={handleOpenModal}>AQUI!</span></p>
-                </div>
-            </div>
+            }
         </div>
     );
 }
